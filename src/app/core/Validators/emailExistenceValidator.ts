e3ab5dbbf90952795af15755d126759ed540eda1
xjanus12x <albertovillacarlos07@gmail.com>
2023-12-16 12:02:28 +0800

    Added asyn validator for checking if user already exist

---
+++ src/app/core/Validators/emailExistenceValidator.ts
@@ -0,0 +1,29 @@
+// validators.ts
+import {
+  AbstractControl,
+  ValidationErrors,
+  AsyncValidatorFn,
+} from '@angular/forms';
+import { BackendService } from '../services/backend.service';
+import { catchError, debounceTime, map, switchMap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+export function emailExistenceValidator(
+  backendService: BackendService
+): AsyncValidatorFn {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    const value: string = control.value || '';
+    return of(value).pipe(
+      debounceTime(300),
+      switchMap((email) => {
+        backendService.setUnconfirmedEmail(email);
+        return backendService.checkEmailExistence().pipe(
+          map((resultData: any) => {
+            return resultData.exists ? { emailExist: true } : null;
+          }),
+          catchError(() => of(null)) // Handling errors, returning null for simplicity
+        );
+      })
+    );
+  };
+}

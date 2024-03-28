b87cc4cd29afa8bab34b55e849cc318d11f49482
xjanus12x <albertovillacarlos07@gmail.com>
2024-03-28 12:44:08 +0800

    toml file updated

--- backend/server.js
+++ backend/server.js
@@ -87,7 +87,6 @@
 //   else console.log("Started... 8085");
 // });
 
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
@@ -155,12 +154,9 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
-
-
-
-
-
+server.post("/api/test", (req, res) => {
+  res.send("hello");
+});
 
 // Add new user
 server.post("/api/user/register", async (req, res) => {
